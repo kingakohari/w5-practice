58338c0dbaa1419f049ba5db30b8dca6b3756e17
Kinga Kohari <kinguca@yahoo.com>
2022-02-16 18:22:05 +0100

    Movie data cards created

--- frontend/script.js
+++ frontend/script.js
@@ -94,7 +94,7 @@ g.myMethod() */
 }
 window.addEventListener("load", loadEvent) */
 
-let h = {
+/* let h = {
     anotherMethod: function (text){
         return `You sent the following argument to this method: ${text}`
     }
@@ -102,4 +102,84 @@ let h = {
 
 let i = h.anotherMethod("argument")
 console.log(i);
-console.log(h.anotherMethod("another argument"));
+console.log(h.anotherMethod("another argument")); */
+
+/* let myString = "Mikkamakka"
+let mySecondString = myString;
+let myThirdString = "Mikkamakka"
+
+console.log(myString);
+console.log(mySecondString);
+console.log(myString===mySecondString);
+console.log(myString===myThirdString); */
+
+/* let myObject = {
+    myString: "Mikkamakka"
+}
+
+let mySecondObject = myObject;
+
+let myThirdObject = {
+    myString: "Mikkamakka"
+}
+
+mySecondObject.myString = "domdodom";
+
+let myFourthObject = {...myObject}
+
+myFourthObject.myString = "Vacskamati"
+
+console.log(myObject.myString);
+console.log(myFourthObject.myString); */
+
+/* console.log(myObject === mySecondObject);
+console.log(myObject === myThirdObject); */
+
+
+/* console.log(""==="");
+console.log({}==={}); */
+
+/* window. addEventListener("load", function(){
+    console.log("My first function") -> anonymous function
+}) */
+
+function loadEvent() {
+    console.log("My second function")
+
+    let card2 = function (title, year, rate){
+        return `
+    <div class="card">
+            <h2>${title}</h2>
+            <time>${year}</time>
+            <p>${rate}</p>
+       </div>
+    `;
+};
+
+    let favMovie = {
+        "title": "Moulin Rouge",
+        "year": 2004,
+        "rate": 9.9
+    }
+
+    rootElement.insertAdjacentHTML("beforeend", card2(favMovie.title, favMovie.year, favMovie.rate));
+
+    let anotherFavMovie = {
+            "title": "The Last Scoutboy",
+            "year": 1991,
+            "rate": 7.0
+    }
+    
+    rootElement.insertAdjacentHTML("beforeend", card2(anotherFavMovie.title, anotherFavMovie.year, anotherFavMovie.rate));
+
+    for (const movieSend of movies) {
+
+        /* movie.title = "Hello!" */
+
+       rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, movieSend.rate));
+    }
+    
+    console.log(movies);
+}
+
+window.addEventListener("load", loadEvent)
