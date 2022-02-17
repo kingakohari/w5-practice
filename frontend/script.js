ea8b050a6ed2b2c3b216679ca4ee0727d57262b6
Kinga Kohari <kinguca@yahoo.com>
2022-02-17 19:57:00 +0100

    New method for rendering list

--- frontend/script.js
+++ frontend/script.js
@@ -1,53 +1,54 @@
 
 function loadEvent() {
-    let rootElement = document.getElementById("root");
+ 
+    let rootElement = document.getElementById("root")
 
-    let card2 = function (title, year, rate){
-        
-            return `
-            <div class="card">
-                <h2>${title}</h2>
-                <time>${year}</time>
-                <p>${rate}</p>
-            </div>
-            `;
+
+    let card2 = function(title, year, rate){
+        return `
+        <div class="card">
+            <h2>${title}</h2>
+            <div class="time">${year}</div>
+            <div class="rate">${rate}</div>
+        </div>
+        `;
     }
 
-    let renderAllCardElements = function(incomingMoviesArray) {
+    let renderAllCardElements = function(incomingMoviesArray){
 
-        let renderedCardList = `<div class="cards">`; 
-        
-        //for ciklus végigmegy az incoming movies array-n//
-        
-        for (const incomingMovie of incomingMoviesArray){
+        let renderedCardList = `<div class="cards">`;
 
-            renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate) 
+        //for ciklus végigmegy a cards array-n//
+       
+        for (const incomingMovie of incomingMoviesArray) {
+
+            renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate)
+
         }
-
         //for ciklus minden lépcsőjénél hozzáadja a renderedCardList-hez az adott elemet a megfelelő div card2-ben//
 
         renderedCardList += `</div>`
 
-        console.log(renderedCardList);
-        
-        //returnöli az elkészült elemekkel feltöltött cardList-et stringek formájában//
-        return renderedCardList;
+        console.log(renderedCardList)
+
+        return renderedCardList //ez stringeket ad vissza//
     }
+
 
     let newGoodMovies = [];
 
     for (const movieSend of movies) {
-
-
         if (movieSend.year > 2000 && movieSend.rate >= 8) {
 
             newGoodMovies.push(movieSend);
-        }
-        
-    newGoodMovies.sort(function(a, b){return a.year - b.year});
 
-    rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies)) // beküldi a renderAllCardElementhez 
+        }
     }
+    newGoodMovies.sort(function(a, b){return a.year - b.year})
+
+    rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies));
+
+    // beküldi a renderAllCardElement-hez 
 }
 
 window.addEventListener("load", loadEvent);
