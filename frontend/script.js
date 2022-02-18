105b88fb36c7f8231db96149807e65e788ebaaad
Kinga Kohari <kinguca@yahoo.com>
2022-02-18 11:24:50 +0100

    Grid template editing

--- frontend/script.js
+++ frontend/script.js
@@ -2,8 +2,8 @@ function loadEvent(){
 
     let rootElement = document.getElementById("root")
 
-    let card = function(title, year, rate){`
-        return <div class="card">
+    let card = function(title, year, rate){
+        return `<div class="card">
             <h2>${title}</h2>
             <h4>${year}</h4>
             <h5>${rate}</h5>
